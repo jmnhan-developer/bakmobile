089a97b74678451b4b0dbecf35b0d72671d5b0df
Jean-Marc Nhan <jmnhan@yahoo.fr>
2020-12-07 09:50:29 +0100

    first commit

---
+++ frontend/picto.js
@@ -0,0 +1,51 @@
+//Picto Home de la barre de navigation
+import { FontAwesome } from '@expo/vector-icons'; 
+<FontAwesome name="home" size={24} color="black" />
+
+//Picto Search de la barre de navigation
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="search" size={24} color="black" />
+
+//Picto + de la barre de naviation pour mettre une annonce
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="plus" size={24} color="black" />
+
+//Picto User (profil) de la barre de navigation
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="user-o" size={24} color="black" />
+
+//Picto User (photo user par défaut)
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="user-circle-o" size={24} color="black" />
+
+//Picto Star (vide) pour l'evaluation des acheteurs/vendeurs
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="star-o" size={24} color="black" />
+
+//Picto Star (plein) pour l'évaluation des acheteurs/vendeus dans l'ecran profil
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="star" size={24} color="black" />
+
+//Picto Coeur pour les likes (wishlist et favoris)
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="heart-o" size={24} color="black" />
+
+//Picto Enveloppe pour la messagerie
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="envelope-o" size={24} color="black" />
+
+//Picto Porte Monnaie pour l'ecran de profil
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="euro" size={24} color="black" />
+
+//Picto Camera pour prise de photo
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="camera" size={24} color="black" />
+
+//Picto Arrow pour revenir à l'écran précédent
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="long-arrow-left" size={24} color="black" />
+
+//Picto "mes ventes et mes achats"
+import { FontAwesome } from '@expo/vector-icons';
+<FontAwesome name="shopping-cart" size={24} color="black" />
