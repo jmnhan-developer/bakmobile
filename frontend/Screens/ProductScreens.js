7daf2c7ce59d922283995823350122168010ef5b
axelbrt <axel.barateau@gmail.com>
2020-12-07 15:43:25 +0100

    porductScreen

---
+++ frontend/Screens/ProductScreens.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// import { StyleSheet, Text, View } from 'react-native';
+
+
+// function ProductScreens(){
+//     return (
+//         <View>
+//             <Text>
+//                 ProductScreens
+//             </Text>
+//         </View>
+//     )
+// }
+
+// export default (ProductScreens);
