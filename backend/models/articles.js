7635c6e961564291ad55fd509bc2beeee5f4955a
Paul <paulhugo@amazon.fr>
2020-12-07 16:37:09 +0100

    adding first routes for backend, still to complete routes based on work from frontend

---
+++ backend/models/articles.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const articleSchema = mongoose.Schema({
+    title:String,
+    description:String,
+    brand:String,
+    kidsAge:String,
+    price:String,
+    shippingFees:String,
+    category:String,
+    subcategory:String,
+    state:String,
+    articleSold:Boolean,
+    sellerID:String,
+    images:String,
+    creationDate:Date
+})
+
+const articleModel = mongoose.model('articles',articleSchema)
+
+module.exports = articleModel
