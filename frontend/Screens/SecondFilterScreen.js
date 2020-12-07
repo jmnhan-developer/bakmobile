7be7e92f4b673cf45d80a20459e0be452ca9297a
felix <fel93@live.fr>
2020-12-07 15:23:30 +0100

    deuxieme commit second Filtre Felix

--- frontend/Screens/SecondFilterScreen.js
+++ frontend/Screens/SecondFilterScreen.js
@@ -1,54 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, FlatList,SafeAreaView } from 'react-native';
-import SecondFilterScreen from './SecondFilterScreen'
-export default function FilterScreen() {
+
+export default function SecondFilterScreen() {
+
+
 
     const DATA = [
         {
-            title: 'Se déplacer',
-            color:'green'
+            title: 'Siége auto',
         },
         {
-            title: 'S habiller',
-            color:'orange'
+            title: 'Poussettes',
         },
         {
-            title: 'Se baigner',
-            color:'orange'
+            title: 'Nacelles',
         },
    
         {
-            title: 'Dormir',
-            color:'orange'
+            title: 'Portes-bébés',
           },
           {
-            title: 'Accessoires',
-            color:'orange'
+            title: 'Sacs à langer',
 
           },
           {
-            title: 'Autre',
-            color:'orange'
+            title: 'Chanceliére',
+          },
+          {
+            title: 'Habillage',
+
+          },
+          {
+            title: 'Autres',
           }
       ];
 
 
       const Item = ({ title }) => (
         <View style={styles.item}>
-          <Text style={styles.title} >{title}</Text>
+          <Text style={styles.title}>{title}</Text>
         </View>
       );
 
       
         const renderItem = ({ item }) => (
-   <Item title={item.title} />
+          <Item title={item.title} />
         )
 
 
   return (
       <View>
-        <Text style={styles.titlePage}>Choisissez une catégorie</Text>
+        <Text style={styles.titlePage}>Choisissez une sous-catégorie</Text>
     
          <View style={styles.container}>
       
@@ -57,21 +60,26 @@ export default function FilterScreen() {
            horizontal={true}
            data={DATA.slice(0,2)}
            renderItem={renderItem}
-           
+           keyExtractor={item => item.id}
            />
            <FlatList
            horizontal={true}
            data={DATA.slice(2,4)}
            renderItem={renderItem}
-           
+           keyExtractor={item => item.id}
            />
            <FlatList
            horizontal={true}
            data={DATA.slice(4,6)}
            renderItem={renderItem}
-           
+           keyExtractor={item => item.id}
            />
-          <SecondFilterScreen/>
+           <FlatList
+           horizontal={true}
+           data={DATA.slice(6,8)}
+           renderItem={renderItem}
+           keyExtractor={item => item.id}
+           />
           </SafeAreaView>
     </View>
 
@@ -88,7 +96,7 @@ const styles = StyleSheet.create({
   },
   titlePage:{
       marginTop:40,
-      marginLeft:75,
+      marginLeft:55,
       fontSize:20
   },
   item: {
