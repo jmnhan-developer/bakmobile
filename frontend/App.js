34de0cffa32c67db8073e8918d4e55d8f86a9251
cédric <cedric_lecalvez@hotmail.com>
2020-12-07 18:30:39 +0100

    replacing package json dependancies

--- frontend/App.js
+++ frontend/App.js
@@ -1,55 +1,164 @@
-import React from 'react';
-import { FlatList, SafeAreaView, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React, { useState, useEffect, useRef} from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import {connect} from 'react-redux';
 
-const styles = StyleSheet.create({
-    container: {
-     flex: 1,
-     paddingTop: 150,
-     color:'#D6A2E8',
-     fontFamily: 'sans-serif-light',
-    },
-    item: {
-      paddingTop: 10,
-      fontFamily: 'sans-serif-light',
-      fontSize: 18,
-      height: 44,
-      borderBottomColor: '#82589F',
-      color:'#82589F',
-      borderBottomWidth:1,
-    },
-    icon: {
-      padding:300,
-    }
-  });
+import { Camera } from 'expo-camera';
+
+
+import { withNavigationFocus } from 'react-navigation';
+
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+import IconIonic from 'react-native-vector-icons/Ionicons';
+
+import {Button, Overlay} from 'react-native-elements';
+
+function SnapScreen(props) {
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.torch);
   
-  const Item = ({ title }) => (
-    <View style={styles.item}>
-      <Text>{title}</Text>
+  var camera = useRef(null);
+ 
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {  
+    (async () => {
+        const { status } = await Camera.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+    })();
+  }, []);
+  
+  var cameraDisplay;
+  if(hasPermission && props.isFocused){
+    cameraDisplay = <Camera 
+      style={{ flex: 1 }}
+      type={type}
+      flashMode={flash}
+      ref={ref => (camera = ref)}
+    >
+       <View    
+        style={{
+          flex: 1,
+          backgroundColor: 'transparent',
+          flexDirection: 'row',
+        }}>
+          <TouchableOpacity
+            style={{
+            
+                alignSelf: 'flex-end',
+                alignItems: 'center',
+            }}
+            onPress={() => {
+                setType(
+                    type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+            }}
+          >
+           <IconIonic
+            name="md-reverse-camera"
+            size={20}
+            color="#ffffff"
+            /><Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+          </TouchableOpacity>
+
+           <TouchableOpacity
+            style={{
+            
+                alignSelf: 'flex-end',
+                alignItems: 'center',
+            }}
+            onPress={() => {
+                setFlash(
+                  flash === Camera.Constants.FlashMode.torch
+                    ? Camera.Constants.FlashMode.off
+                    : Camera.Constants.FlashMode.torch
+                );
+              }}
+            >
+            <IconFontAwesome
+            name="flash"
+            size={20}
+            color="#ffffff"
+            /><Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flash </Text>
+           </TouchableOpacity>
+
+        </View>
+    </Camera>
+  } else {
+    cameraDisplay = <View style={{ flex: 1 }}></View>
+  }
+
+ 
+  
+
+  return (
+    <View style={{flex: 1}}>
+        <Overlay isVisible={visible}  width="auto" height="auto">
+            <Text>Loading</Text>
+        </Overlay>
+        
+        {cameraDisplay}
+        <Button
+            icon={
+                <IconFontAwesome
+                name="save"
+                size={20}
+                color="#ffffff"
+                />
+            } 
+            title="Snap"
+            buttonStyle={{backgroundColor: "#009788"}}
+            type="solid"
+            onPress={async () => { 
+                setVisible(true);
+                if (camera) {
+                    let photo = await camera.takePictureAsync({quality : 0.7});
+                    setVisible(false);
+                    // console.log(photo)
+
+                    var data = new FormData();
+                    data.append('avatar', {
+                      uri: photo.uri,
+                      type: 'image/jpeg',
+                      name: 'avatar.jpg',
+                    });
+                    const dataPhoto = await fetch("http://172.17.1.116:3000/upload", {
+                    method: 'POST',
+                    body: data
+                    })
+                    
+                    const bodyImage = await dataPhoto.json()
+                    console.log("bodyImage",bodyImage)
+                    // console.log(bodyImage)
+                    props.onIncreaseClick(bodyImage.url)
+                    // if(body.result == true){
+                    //   props.addToken(body.token)
+                    //   setUserExists(true)
+                      
+                    // } else {
+                    //   setErrorsSignup(body.error)
+                    // }
+                  }   
+            }}
+        />
+
     </View>
   );
-  
-  const DATA=[
-    {label: 'Mon Profil',icon:'user-circle-o'},
-    {label: 'Mon évaluation',icon:'star'},
-    {label: 'Mes favoris',icon:'heart-o'},
-    {label: 'Mes articles en vente',icon:'shopping-cart'},
-    {label: 'Mes articles achetés',icon:'shopping-cart'},
-    {label: 'Porte Monnaie',icon:'euro'},
-  ]
+}
 
-  const ProfileMenuScreen = () => {
-    const renderItem = ({ item }) => (
-      <Item title={item.label} />
-    );
-    return (
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-        />
-      </SafeAreaView>
-    );
+function mapDispatchToProps(dispatch) {
+  return {
+    onIncreaseClick: function(url) { 
+        dispatch( {type: 'increase', photoUrl: url } ) 
+    }
   }
-  
-  export default ProfileMenuScreen;
+}
+
+export default connect(
+    null, 
+    mapDispatchToProps
+)(withNavigationFocus(SnapScreen));
+
