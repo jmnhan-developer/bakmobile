5044c53b514b92a48f84af91ab94e068871349b4
cédric <cedric_lecalvez@hotmail.com>
2020-12-08 14:43:51 +0100

    addPicScreen input vers BDD ok

--- frontend/App.js
+++ frontend/App.js
@@ -1,115 +1,127 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, ScrollView, Text, View, Picker,TouchableOpacity } from 'react-native';
+import {Button, Input} from 'react-native-elements';
+import { FontAwesome } from '@expo/vector-icons';
 
 
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import React, {useState} from 'react';
-import { View, Text, StyleSheet, ScrollView, Image,FlatList, SafeAreaView, TouchableOpacity} from 'react-native';
-import { Card, ListItem, Button,  } from 'react-native-elements'
+export default function App(props) {
+  const [selectedValue, setSelectedValue] = useState("");
+  // console.log("selectedValue", selectedValue)
+  const [titleInput , setTitleInput ] = useState("");
+  console.log("title", titleInput)
+  const [desc , setDesc ] = useState("");
+  const [price , setPrice ] = useState("");
+  const [shippingFees , setShippingFees ] = useState("");
 
-import RNPickerSelect from 'react-native-picker-select';
 
-import SellScreen from './Screens/SellScreen'
-import FilterScreen from './Screens/FilterScreen'
-import HomeScreen from './Screens/HomeScreen'
-import MessageScreen from './Screens/MessageScreen'
-import ProfileScreen from './Screens/ProfileScreen'
-import Icon from 'react-native-vector-icons/FontAwesome';
-import createAppContainer  from 'react-navigation';
-import createBottomTabNavigator from 'react-navigation-tabs';
-import createStackNavigator from 'react-navigation-stack';
 
+  var handleClick = async () => {
+    
+    console.log('ceci est le titre post handleclick=', titleInput);
+    const dataArticle = await fetch("http://172.17.1.123:3000/articles/create-article", {
+      method: 'POST',
+      headers: {'Content-Type':'application/x-www-form-urlencoded'},
+      body: `title=${titleInput}&desc=${desc}&price=${price}&shippingFees=${shippingFees}`
+    });
+                               
+    console.log(dataArticle)
+    const dataAnnonce = await dataArticle.json()
+    console.log("dataAnnonce", dataAnnonce)
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView style={{width: '90%'}}>
+
+        <Text style={{marginTop:40,textAlign: 'center'}}>Jusqu'à 6 photos</Text>
+
+        <Button
+          buttonStyle={{marginTop:40, marginBottom:40}}
+          icon={
+              <FontAwesome name="camera" size={24}  color="#009788"/>
+              }            
+          title="Ajouter des photos"
+          type="outline"
+          onPress={() => {handleClick()}}
+        />
+      
+        <Input style = {{ width: '90%'}}
+          placeholder='Titre'
+          onChangeText={(val) => setTitleInput(val)}
+        />
+        <Input style = {{ width: '90%'}}
+          placeholder="Descrition de l'annonce"
+          onChangeText={(val) => setDesc(val)}
+        />
+        <Input style = {{ width: '90%'}}
+          placeholder='Prix'
+          onChangeText={(val) => setPrice(val)}
+        />
+        <Input style = {{ width: '90%'}}
+          placeholder='Frais de port'
+          onChangeText={(val) => setShippingFees(val)}
+        />
+
+        
+        <Picker
+          selectedValue={selectedValue}
+          style={{ width: '70%'}}
+          onValueChange={(itemValue) => setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Catégorie" value="Catégorie" />
+          <Picker.Item label="Se déplacer" value="Se déplacer" />
+          <Picker.Item label="Manger" value="Manger" />
+          <Picker.Item label="Dormir" value="Dormir" />
+        </Picker>
+
+        <Picker
+          selectedValue={selectedValue}
+          style={{ width: '70%'}}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Sous catégorie" value="Sous catégorie" />
+          <Picker.Item label="Poussettes" value="Poussettes" />
+          <Picker.Item label="Nacelle" value="Nacelle" />
+        </Picker>
+        <Picker
+          selectedValue={selectedValue}
+          style={{ width: '70%'}}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Marque" value="Marque" />
+          <Picker.Item label="Yoyo" value="Yoyo" />
+          <Picker.Item label="Bébé Confort" value="Bébé Confort" />
+        </Picker>
+        <Picker
+          selectedValue={selectedValue}
+          style={{ width: '70%'}}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Etat" value="Etat" />
+          <Picker.Item label="Neuf" value="Neuf" />
+          <Picker.Item label="Bon Etat" value="Bon Etat" />
+          <Picker.Item label="Etat d'usage" value="Etat d'usage" />
+        </Picker>
+
+        <Button            
+          title="Ajoute ton annonce"
+          type="solid"
+          buttonStyle={{backgroundColor: "#009788"}}
+          onPress={() => handleClick()}
+        />
+
+      </ScrollView>
+
+  </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-     flex: 1,
-     paddingTop: 150,
-     color:'#D6A2E8',
-     fontFamily: 'sans-serif-light',
-    },
-    item: {
-      paddingTop: 10,
-      fontFamily: 'sans-serif-light',
-      fontSize: 18,
-      height: 44,
-      borderBottomColor: '#82589F',
-      color:'#82589F',
-      borderBottomWidth:1,
-    },
-    icon: {
-      padding:300,
-    }
-  });
-  
-  const Item = ({ title }) => (
-    <View style={styles.item}>
-      <Text>{title}</Text>
-    </View>
-  );
-  
-  const DATA=[
-    {label: 'Mon Profil',icon:'user-circle-o'},
-    {label: 'Mon évaluation',icon:'star'},
-    {label: 'Mes favoris',icon:'heart-o'},
-    {label: 'Mes articles en vente',icon:'shopping-cart'},
-    {label: 'Mes articles achetés',icon:'shopping-cart'},
-    {label: 'Porte Monnaie',icon:'euro'},
-  ]
-
-var BottomNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Chercher: FilterScreen,
-  Vente: SellScreen,
-  Message:MessageScreen,
-  Profile:ProfileScreen
-
-},
-  {
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ tintColor }) => {
-        var iconName;
-        if (navigation.state.routeName == 'Home') {
-          iconName = 'home';
-        } else if (navigation.state.routeName == 'Chercher') {
-          iconName = 'search';
-        } else if  (navigation.state.routeName == 'Vente') {
-          iconName = 'plus';
-        } else if (navigation.state.routeName == 'Message') {
-          iconName = 'envelope-o';
-        } else if (navigation.state.routeName == 'Profile') {
-          iconName = 'user-o';
-        }
-
-        return <FontAwesome name={iconName} size={24} color={tintColor} />;
-      },
-    }),
-    tabBarOptions: {
-      activeTintColor: '#eb4d4b',
-      inactiveTintColor: '#FFFFFF',
-      style: {
-        backgroundColor: '#130f40',
-      }
-    }
-   
-
-  });
-
-
-// export default Navigation = createAppContainer(BottomNavigator);
-
-  const ProfileMenuScreen = () => {
-    const renderItem = ({ item }) => (
-      <Item title={item.label} />
-    );
-    return (
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-        />
-        {BottomNavigator}
-      </SafeAreaView>
-    );
-  }
-  
-  export default ProfileMenuScreen;
-
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
