67624ae6fe3018c84e977a0b750f43ffdd3620c0
Jean-Marc Nhan <jmnhan@yahoo.fr>
2020-12-09 11:06:36 +0100

    screenswork

--- frontend/App.js
+++ frontend/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { FontAwesome } from '@expo/vector-icons';
-import { View, KeyboardAvoidingView, Text, StyleSheet, ScrollView } from 'react-native';
-import { Button, Input, Card, SearchBar, Image } from 'react-native-elements';
+import { KeyboardAvoidingView, StyleSheet, ScrollView } from 'react-native';
+import { Input, Card, SearchBar, Image } from 'react-native-elements';
 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, FlatList,SafeAreaView } from 'react-native';
+import {Text, View, Button, FlatList, SafeAreaView } from 'react-native';
 import FilterScreen from './Screens/FilterScreen'
 import ProfileScreen from './Screens/ProfileScreen'
 import SellScreen from './Screens/SellScreen'
 import BasketScreens from './Screens/BasketScreens'
 import ProductScreens from './Screens/ProductScreens'
 import {createBottomTabNavigator} from 'react-navigation-tabs'
-import { FontAwesome } from '@expo/vector-icons'; 
 import {createAppContainer } from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import HomeScreens from './Screens/HomeScreens'
@@ -24,112 +23,53 @@ import {createStore, combineReducers}  from 'redux';
 
 const store = createStore(combineReducers({photo}));
 
-var StackNavigator = createStackNavigator({ 
-
-function FilterScreens() {
+var StackNavigator = createStackNavigator({
   
-  return (
-    <View style={{flex: 1, marginTop:20}}>
+  Filter: FilterScreen,
+  Product: ProductScreens,
+  Result: ResultScreens,
+  Basket: BasketScreens
+},
+{headerMode: 'none'}
+);
 
-      <Text style={{fontSize:15, textAlign:"center", justifyContent: "center", marginTop:5, marginBottom:5}}>Sélectionner le besoin de votre enfant</Text>
-
-      <View style={{flex:1, flexDirection:"row", width:"95%", marginLeft:10, alignItems:"center" , justifyContent:"space-between", flexWrap: 'wrap'}}>
-
-        <Button style={{marginTop:10, width:190}}
-          title="Se Déplacer"
-          buttonStyle={{ backgroundColor: '#B3B6B7'}}
-          type="solid"
-        />
-        <Button style={{marginTop:10, width:190}}
-          title="S'habiller"
-          buttonStyle={{ backgroundColor: "#B3B6B7"}}
-          type="solid"
-        />
-        <Button style={{marginTop:10, width:190}}
-              title="Se Baigner"
-              buttonStyle={{ backgroundColor: "#B3B6B7"}}
-              type="solid"
-            />
-        <Button style={{marginTop:10, width:190}}
-          title="Dormir"
-          buttonStyle={{ backgroundColor: "#B3B6B7"}}
-          type="solid"
-        />
-        <Button style={{marginTop:10, width:190}}
-              title="Manger"
-              buttonStyle={{ backgroundColor: "#B3B6B7"}}
-              type="solid"
-            />
-        <Button style={{marginTop:10, width:190}}
-          title="Jouer"
-          buttonStyle={{ backgroundColor: "#B3B6B7"}}
-          type="solid"
-        />
-        <Button style={{marginTop:10, width:190}}
-              title="Accessoires"
-              buttonStyle={{ backgroundColor: "#B3B6B7"}}
-              type="solid"
-            />
-        <Button style={{marginTop:10, width:190}}
-          title="Autres"
-          buttonStyle={{ backgroundColor: "#B3B6B7"}}
-          type="solid"
-        />
-      </View>
-    </View>
-  )
-}
-
-export default FilterScreens;
-
-
-// import React, { useReducer,useState } from 'react';
-// import { StyleSheet, Text, View, Button, FlatList,SafeAreaView } from 'react-native';
-// import FilterScreen from './Screens/FilterScreen'
-// import ProfileScreen from './Screens/ProfileScreen'
-// import SellScreen from './Screens/SellScreen'
-// import {createBottomTabNavigator} from 'react-navigation-tabs'
-// import { FontAwesome } from '@expo/vector-icons'; 
-// import {createAppContainer } from 'react-navigation';
-
-
-
-
-// var BottomNavigator = createBottomTabNavigator({
+var BottomNavigator = createBottomTabNavigator({
   
-//   Sell: SellScreen,
-//   Filter:FilterScreen,
-//   Profile: ProfileScreen,
+  Sell: SellScreen,
+  Filter:FilterScreen,
+  Profile: ProfileScreen,
   
-// },
-//   {
-//     defaultNavigationOptions: ({ navigation }) => ({
-//       tabBarIcon: ({ tintColor }) => {
-//         var iconName;
-//         if (navigation.state.routeName == 'Sell') {
-//           iconName = 'plus';
-//         } else if (navigation.state.routeName == 'Filter') {
-//           iconName = 'search';
-//         } else if (navigation.state.routeName == 'Profile') {
-//           iconName = 'user-o';
-//         }
-//         return <FontAwesome name={iconName} size={25} color={tintColor} />;
-//       },
-//     }),
-//     tabBarOptions: {
-//       activeTintColor: '#eb4d4b',
-//       inactiveTintColor: '#FFFFFF',
-//       style: {
-//         backgroundColor: '#130f40',
-//       }
-//     }
+},
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        var iconName;
+        if (navigation.state.routeName == 'Home') {
+          iconName = 'home';
+        } else if (navigation.state.routeName = 'sell'){
+          iconName = 'plus';
+        } else if (navigation.state.routeName == 'Filter') {
+          iconName = 'search';
+        } else if (navigation.state.routeName == 'Profile') {
+          iconName = 'user-o';
+        }
+        return <FontAwesome name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#eb4d4b',
+      inactiveTintColor: '#FFFFFF',
+      style: {
+        backgroundColor: '#130f40',
+      }
+    }
    
 
-//   });
-//   const Navigation = createAppContainer(BottomNavigator);
+  });
+  const Navigation = createAppContainer(BottomNavigator);
 
-//   export default function App () {
-//     return (
-//      <Navigation/>
-//       )
-//   }
+  export default function App () {
+    return (
+     <Navigation/>
+      )
+  }
