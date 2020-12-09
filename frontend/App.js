70775633978ca9d7d318c7139df0c96be39cbefb
axelbrt <axel.barateau@gmail.com>
2020-12-09 16:33:06 +0100

    test 1633

---
+++ frontend/App.js
@@ -0,0 +1,98 @@
+<<<<<<< HEAD
+=======
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList,SafeAreaView } from 'react-native';
+import FilterScreen from './Screens/FilterScreen'
+import ProfileScreen from './Screens/ProfileScreen'
+import SellScreen from './Screens/SellScreen'
+import BasketScreens from './Screens/BasketScreens'
+import ProductScreens from './Screens/ProductScreens'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import { FontAwesome } from '@expo/vector-icons'; 
+import {createAppContainer }  from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import HomeScreens from './Screens/HomeScreens'
+import ResultScreens from './Screens/ResultScreens';
+<<<<<<< HEAD
+
+import product from '../frontend/reducers/Article.reducer';
+=======
+import AddPicScreen from './Screens/AddPicScreen'
+import ProfileMenuScreen from './Screens/ProfileMenuScreen'
+
+>>>>>>> ad52b48eec0c50495b22e233fa3be517dd66549e
+import photo from '../frontend/reducers/Pic.reducer';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers}  from 'redux';
+import ProfileBoughtArticleScreen from './Screens/ProfileBoughtArticleScreen'
+import ProfileSellingArticleScreen from './Screens/ProfileSellingArticleScreen'
+
+
+const store = createStore(combineReducers({photo,product}));
+
+var StackNavigator = createStackNavigator({ 
+
+  
+  Filter:  FilterScreen,  
+  Product: ProductScreens,
+  Result:ResultScreens,
+  Basket: BasketScreens,
+  ProfileUser:ProfileScreen,
+  ArticleBought: ProfileBoughtArticleScreen,
+  ArticleSell: ProfileSellingArticleScreen
+
+}, 
+{headerMode: 'none'}
+);  
+
+var BottomNavigator = createBottomTabNavigator({
+  
+  Home:HomeScreens,
+  Sell: SellScreen,
+  Filter: StackNavigator,
+  Profile: ProfileMenuScreen,
+  
+ },
+  {
+   defaultNavigationOptions: ({ navigation }) => ({
+     tabBarIcon: ({ tintColor }) => {
+      var iconName;
+         if (navigation.state.routeName == 'Home') {
+        iconName = 'home';
+       }
+         else if (navigation.state.routeName == 'Sell') {
+        iconName = 'plus';
+       } else if (navigation.state.routeName == 'Filter') {
+         iconName = 'search';
+       } else if (navigation.state.routeName == 'Profile') {
+         iconName = 'user-o';
+      }
+       return <FontAwesome name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+     tabBarOptions: {
+      activeTintColor: '#eb4d4b',
+      inactiveTintColor: '#FFFFFF',
+      style: {
+       backgroundColor: '#130f40',
+      }
+    }
+   
+
+  });
+
+ 
+
+
+const Navigation = createAppContainer(BottomNavigator);
+
+export default function App(){
+    return (
+
+      <Provider store={store}>
+        <Navigation/>
+      </Provider>
+
+      )
+}
+>>>>>>> 459122b17af90147d313c46bd9c0b3c4ecb3f4a4
